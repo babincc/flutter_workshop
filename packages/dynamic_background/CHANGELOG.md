a6d2f36194f38f70d07bdeb89771e05b5bfd74d4
Christian Babin <cssbabin@gmail.com>
2024-02-09 02:18:02 -0500

    Small update to Dynamic BG

--- packages/dynamic_background/CHANGELOG.md
+++ packages/dynamic_background/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1 - February 9, 2024
+
+- Documentation fix
+
 ## 0.1.0 - February 9, 2024
 
 - Initial release
--- packages/dynamic_background/lib/dynamic_background.dart
+++ packages/dynamic_background/lib/dynamic_background.dart
@@ -1,5 +1,5 @@
 // @author Christian Babin
-// @version 0.1.0
+// @version 0.1.1
 // https://github.com/babincc/flutter_workshop/blob/master/packages/dynamic_background/lib/dynamic_background.dart
 
 library dynamic_background;
