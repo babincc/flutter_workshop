dac5d33fd8e47a804f5c9d7951db06317ecae699
Christian Babin <cssbabin@gmail.com>
2025-01-13 12:52:45 -0500

    Minor fix to MySkeleton

--- my_skeleton/lib/main.dart
+++ my_skeleton/lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:my_skeleton/firebase_options.dart';
 import 'package:my_skeleton/my_app.dart';
+import 'package:my_skeleton/utils/my_file_explorer.dart';
 
 Future<void> main() async {
   usePathUrlStrategy();
 
   // Initialize app.
   WidgetsFlutterBinding.ensureInitialized();
+  await MyFileExplorer().ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
